8b5b75369a6d4208b28457856de1d5bb1e48b049
JinalJBhanushali <jsgajra@gmail.com>
2026-07-03 14:11:37 +0530

    commit

---
+++ backend/Fenik.API/Controller/InventoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fenik.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+    }
+}
